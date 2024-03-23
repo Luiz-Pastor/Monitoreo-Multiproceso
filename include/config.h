e928c5f4b4b041f8ef4bf52890b0b79a3958a21c
Luiz-Pastor <luigpacr5@gmail.com>
2024-03-23 20:29:48 +0100

    Added a common file with the projecy config, such as macros and names. Like a .env file

---
+++ include/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+# define CONFIG_H
+
+/* TODO: Nombre de los mensajes */
+
+# define MIN_LAG 10			/*!< Min lag time */
+
+# define MIN_ROUNDS 1		/*!< Min number of rounds */
+
+# define MAX_THREADS 10		/*!< Number of threads used */
+
+# define INIT_TARGET 0		/*!< Initial target of the search */
+
+# define POW_LIMIT 9997697	/*!< Maximum number for the hash result. */
+
+#endif
