4b4ab674c9c4946326d41a9d4dc4f4f290c48087
Luiz-Pastor <luigpacr5@gmail.com>
2024-03-24 11:46:10 +0100

    Added the msg name and the struct to send a recieve the msg

--- include/config.h
+++ include/config.h
@@ -1,16 +1,23 @@
 #ifndef CONFIG_H
 # define CONFIG_H
 
-/* TODO: Nombre de los mensajes */
+# define MIN_LAG 10					/*!< Min lag time */
 
-# define MIN_LAG 10			/*!< Min lag time */
+# define MIN_ROUNDS 1				/*!< Min number of rounds */
 
-# define MIN_ROUNDS 1		/*!< Min number of rounds */
+# define MAX_THREADS 10				/*!< Number of threads used */
 
-# define MAX_THREADS 10		/*!< Number of threads used */
+# define INIT_TARGET 0				/*!< Initial target of the search */
 
-# define INIT_TARGET 0		/*!< Initial target of the search */
+# define POW_LIMIT 9997697			/*!< Maximum number for the hash result. */
 
-# define POW_LIMIT 9997697	/*!< Maximum number for the hash result. */
+# define MSG_QUEUE_NAME "/miner"	/*!< Name of the message queue */
+
+# define MAX_MSG	6				/*!< Max number of msg on the queue*/
+
+typedef struct e_msg {
+	long	target;
+	long	result;
+} t_msg;
 
 #endif
